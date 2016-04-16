13aa007657fae126b257c450d83d955f5a42a823
idannik <idannik@post.bgu.ac.il>
2016-04-16 15:37:15 +0300

    Init

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 public class QuizActivity extends AppCompatActivity {
     private Button mTrueButton;
     private Button mFalseButton;
-    private Button mNextButton;
+    private Button mNextButton; 
     private int mCurrentIndex;
 
 
